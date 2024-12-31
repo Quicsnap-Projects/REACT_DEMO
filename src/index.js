50d0a27d080a0138bd4a64649463025c4c08d9d6
z004u4pj <devesh.dhote.ext@siemens.com>
2024-12-31 14:30:49 +0530

    code correction

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import UploadImage from "./components/upload_image/UploadImage"
-import AccountSettings from "./components/accountSettings/AccountSettings"
+import AccountSettings from "./components/UserDetail/AccountSettings"
 import UploadProduct from "./components/Train/UploadProduct"
 import ImageEditor from "./components/Canvas/ImageEditor"
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <UploadImage/>
+
   // <AccountSettings/> 
   // <ImageEditor/>
   <BrowserRouter>
